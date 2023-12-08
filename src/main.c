31265a6999f298cfb5c557e6ea78e2faaa94fa27
Jonathan <git@jonathansm.dev>
2023-12-08 13:39:59 -0600

    feat(main): use float instead of real_t for stack storage
    The `real_t` datatype is low precision. Instead of using the `real_t`
    type for storing variables in the stack use floats and only convert to a
    `real_t` as an intermediate step to converting the value to a string.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
+#include "stdint.h"
 #include "stdio.h"
+#include <math.h>
 #include <string.h>
 #include <tice.h>
 
@@ -6,7 +8,9 @@
 #define ti_X ("\x58\0\0")
 #define ti_L1 ("\x5D\x0\0")
 
-real_t stack[101];
+float stack[101]; // We want much more precision than real_t can give us
+
+// real_t stack[101];
 char buffer[50];
 uint8_t idx;
 bool decimal;
@@ -14,7 +18,17 @@ bool negative;
 bool constantsmode = false;
 bool scimode = false;
 bool radians = true;
-real_t decimalfactor;
+float decimalfactor;
+
+float ln10, pi, e;
+
+void init_constants() {
+    ln10 = logf(10);
+    pi = acosf(-1);
+    e = expf(1);
+}
+
+float x, L1[100];
 
 real_t r_0, r_1, r_2, r_3, r_4, r_5, r_6, r_7, r_8, r_9, r_10, r_n1, r_ln10, r_pi, r_e;
 
@@ -36,9 +50,14 @@ void init_real_constants() {
     r_e = os_RealExp(&r_1);
 }
 
+void float_to_str(float f, char *buffer) {
+    real_t r = os_FloatToReal(f);
+    os_RealToStr(buffer, &r, 0, 1, -1);
+}
+
 void draw_line() {
     strcpy(buffer, "                    ");
-    os_RealToStr(buffer, &stack[idx], 0, 1, -1);
+    float_to_str(stack[idx], buffer);
     for (int i = 0; i < 15; i++) {
         os_SetCursorPos(9, i);
         os_PutStrFull(&buffer[i]);
@@ -57,16 +76,12 @@ void draw_stack_clear(uint8_t row, bool clear) {
     if (row >= 9) {
         os_SetCursorPos(8, 0);
         os_PutStrFull("...            ");
-        real_t len = os_Int24ToReal((int24_t)idx);
+        real_t len = os_Int24ToReal((int24_t)idx); // Turn the len into a string
         os_RealToStr(buffer, &len, 0, 1, -1);
         os_SetCursorPos(8, 4);
         os_PutStrFull(buffer);
     } else {
-        if (scimode) {
-            os_RealToStr(buffer, &stack[row], 0, 2, 2);
-        } else {
-            os_RealToStr(buffer, &stack[row], 0, 1, -1);
-        }
+        float_to_str(stack[row], buffer);
         if (clear) {
             os_SetCursorPos(row, 0);
             os_PutStrFull("               ");
@@ -108,7 +123,7 @@ void constants_mode(bool enabled) {
 void new_entry() {
     decimal = false;
     negative = false;
-    stack[idx] = r_0;
+    stack[idx] = 0;
     hint(" ");
     draw_line();
     // The new line doesn't overwrite the second char so manually fix that
@@ -125,21 +140,21 @@ void new_problem() {
 
 #define BINARY_OP(os_func)                                                             \
     do {                                                                               \
-        if (os_RealCompare(&stack[idx], &r_0) != 0) {                                  \
+        if (stack[idx] != 0) {                                                         \
             if (idx >= 1) {                                                            \
-                stack[idx - 1] = os_func(&stack[idx - 1], &stack[idx]);                \
+                stack[idx - 1] = os_func(stack[idx - 1], stack[idx]);                  \
                 draw_stack_clear(idx - 1, true);                                       \
-                os_SetRealListElement(ti_L1, idx, &stack[idx - 1]);                    \
+                L1[idx] = stack[idx - 1];                                              \
                 new_entry();                                                           \
             }                                                                          \
         } else {                                                                       \
             if (idx >= 2) {                                                            \
-                stack[idx - 2] = os_func(&stack[idx - 2], &stack[idx - 1]);            \
+                stack[idx - 2] = os_func(stack[idx - 2], stack[idx - 1]);              \
                 draw_stack_clear(idx - 2, true);                                       \
                 delete_stack(idx - 1);                                                 \
                 idx--;                                                                 \
-                os_SetListDim(ti_L1, idx);                                             \
-                os_SetRealListElement(ti_L1, idx, &stack[idx - 1]);                    \
+                L1[idx];                                                               \
+                L1[idx] = stack[idx - 1];                                              \
                 new_entry();                                                           \
             }                                                                          \
         }                                                                              \
@@ -147,14 +162,14 @@ void new_problem() {
 
 #define UNARY_OP(os_func)                                                              \
     do {                                                                               \
-        if (os_RealCompare(&stack[idx], &r_0) != 0) {                                  \
-            stack[idx] = os_func(&stack[idx]);                                         \
+        if (stack[idx] != 0) {                                                         \
+            stack[idx] = os_func(stack[idx]);                                          \
             draw_line();                                                               \
         } else {                                                                       \
             if (idx >= 1) {                                                            \
-                stack[idx - 1] = os_func(&stack[idx - 1]);                             \
+                stack[idx - 1] = os_func(stack[idx - 1]);                              \
                 draw_stack_clear(idx - 1, true);                                       \
-                os_SetRealListElement(ti_L1, idx, &stack[idx - 1]);                    \
+                L1[idx] = stack[idx - 1];                                              \
                 new_entry();                                                           \
             }                                                                          \
         }                                                                              \
@@ -191,55 +206,63 @@ real_t realLogBase10(real_t *a) {
     return os_RealDiv(&t, &r_ln10);
 }
 
-real_t realSquare(real_t *a) { return os_RealMul(a, a); }
+float square(float a) { return a * a; }
 
-real_t realTenExp(real_t *a) { return os_RealPow(&r_10, a); }
+float tenPow(float a) { return powf(10, a); }
 
-real_t realScientificNotation(real_t *a, real_t *b) {
-    real_t m = os_RealPow(&r_10, b);
-    return os_RealMul(a, &m);
-}
+float scientificNotation(float a, float b) { return a * powf(10, b); }
+
+float add(float a, float b) { return a + b; }
+
+float subtract(float a, float b) { return a - b; }
+
+float multiply(float a, float b) { return a * b; }
+
+float divide(float a, float b) { return a / b; }
+
+float recip(float a) { return powf(a, -1); }
 
 int main() {
     uint8_t key;
 
     init_real_constants();
+    init_constants();
     new_problem();
 
     while (!((key = os_GetCSC()) == sk_Graph || (constantsmode && key == sk_Mode))) {
         if (constantsmode) {
             if (key == sk_Power) {
-                stack[idx] = r_pi;
+                stack[idx] = pi;
                 constants_mode(false);
                 draw_line();
             } else if (key == sk_Div) {
-                stack[idx] = r_e;
+                stack[idx] = e;
                 constants_mode(false);
                 draw_line();
             } else if (key == sk_Store) {
-                os_GetRealVar(ti_X, &stack[idx]);
+                x = stack[idx];
                 constants_mode(false);
                 draw_line();
             } else if (key == sk_Log) {
-                UNARY_OP(realTenExp);
+                UNARY_OP(tenPow);
                 constants_mode(false);
             } else if (key == sk_Ln) {
-                UNARY_OP(os_RealExp);
+                UNARY_OP(expf);
                 constants_mode(false);
             } else if (key == sk_Sin) { // asin and cos no longer switched intentionally
-                UNARY_OP(radDegAsin);   // See CE-Programming/toolchain PR #358
+                UNARY_OP(asinf);        // See CE-Programming/toolchain PR #358
                 constants_mode(false);
             } else if (key == sk_Cos) {
-                UNARY_OP(radDegAcos);
+                UNARY_OP(acosf);
                 constants_mode(false);
             } else if (key == sk_Tan) {
-                UNARY_OP(radDegAtan);
+                UNARY_OP(atanf);
                 constants_mode(false);
             } else if (key == sk_Square) {
-                UNARY_OP(os_RealSqrt);
+                UNARY_OP(sqrtf);
                 constants_mode(false);
             } else if (key == sk_Comma) {
-                BINARY_OP(realScientificNotation);
+                BINARY_OP(scientificNotation);
                 constants_mode(false);
             } else if (key == sk_Apps) {
                 radians = !radians;
@@ -256,98 +279,98 @@ int main() {
                 key == sk_4 || key == sk_5 || key == sk_6 || key == sk_7 ||
                 key == sk_8 || key == sk_9) {
                 if (!decimal) {
-                    stack[idx] = os_RealMul(&stack[idx], &r_10);
-                    real_t toAdd = r_0;
+                    stack[idx] = stack[idx] * 10;
+                    float toAdd = 0;
                     if (key == sk_1)
-                        toAdd = r_1;
+                        toAdd = 1;
                     if (key == sk_2)
-                        toAdd = r_2;
+                        toAdd = 2;
                     if (key == sk_3)
-                        toAdd = r_3;
+                        toAdd = 3;
                     if (key == sk_4)
-                        toAdd = r_4;
+                        toAdd = 4;
                     if (key == sk_5)
-                        toAdd = r_5;
+                        toAdd = 5;
                     if (key == sk_6)
-                        toAdd = r_6;
+                        toAdd = 6;
                     if (key == sk_7)
-                        toAdd = r_7;
+                        toAdd = 7;
                     if (key == sk_8)
-                        toAdd = r_8;
+                        toAdd = 8;
                     if (key == sk_9)
-                        toAdd = r_9;
+                        toAdd = 9;
                     if (!negative)
-                        stack[idx] = os_RealAdd(&stack[idx], &toAdd);
+                        stack[idx] = stack[idx] + toAdd;
                     else
-                        stack[idx] = os_RealSub(&stack[idx], &toAdd);
+                        stack[idx] = stack[idx] - toAdd;
                     draw_line();
                 } else {
-                    real_t toAdd = r_0;
+                    float toAdd = 0;
                     if (key == sk_1)
-                        toAdd = r_1;
+                        toAdd = 1;
                     if (key == sk_2)
-                        toAdd = r_2;
+                        toAdd = 2;
                     if (key == sk_3)
-                        toAdd = r_3;
+                        toAdd = 3;
                     if (key == sk_4)
-                        toAdd = r_4;
+                        toAdd = 4;
                     if (key == sk_5)
-                        toAdd = r_5;
+                        toAdd = 5;
                     if (key == sk_6)
-                        toAdd = r_6;
+                        toAdd = 6;
                     if (key == sk_7)
-                        toAdd = r_7;
+                        toAdd = 7;
                     if (key == sk_8)
-                        toAdd = r_8;
+                        toAdd = 8;
                     if (key == sk_9)
-                        toAdd = r_9;
-                    toAdd = os_RealMul(&toAdd, &decimalfactor);
+                        toAdd = 9;
+                    toAdd = toAdd * decimalfactor;
                     if (!negative)
-                        stack[idx] = os_RealAdd(&stack[idx], &toAdd);
+                        stack[idx] = stack[idx] + toAdd;
                     else
-                        stack[idx] = os_RealSub(&stack[idx], &toAdd);
-                    decimalfactor = os_RealDiv(&decimalfactor, &r_10);
+                        stack[idx] = stack[idx] - toAdd;
+                    decimalfactor = decimalfactor / 10;
 
                     draw_line();
                 }
             } else if (key == sk_Chs) {
-                stack[idx] = os_RealNeg(&stack[idx]);
+                stack[idx] = -(stack[idx]);
                 negative = !negative;
                 draw_line();
             } else if (key == sk_DecPnt) {
                 if (!decimal) {
                     decimal = true;
-                    decimalfactor = os_RealDiv(&r_1, &r_10);
+                    decimalfactor = 0.1;
                     drawdecimal_line();
                 }
             } else if (key == sk_Clear) {
                 new_entry();
             } else if (key == sk_Left) {
                 if (negative)
-                    os_RealNeg(&stack[idx]);
+                    stack[idx] = -(stack[idx]);
                 if (!decimal) {
-                    stack[idx] = os_RealDiv(&stack[idx], &r_10);
+                    stack[idx] = stack[idx] / 10;
                 } else
                     decimal = false;
-                stack[idx] = os_RealFloor(&stack[idx]);
+                stack[idx] = floorf(stack[idx]);
                 if (negative)
-                    os_RealNeg(&stack[idx]);
+                    stack[idx] = -(stack[idx]);
                 draw_line();
             } else if (key == sk_Up) {
                 if (idx >= 1) {
                     delete_stack(idx - 1);
                     stack[idx - 1] = stack[idx];
-                    stack[idx] = r_0;
+                    stack[idx] = 0;
                     idx--;
-                    os_SetListDim(ti_L1, idx);
+                    L1[idx];
                 }
             } else if (key == sk_Enter) {
                 if (idx >= 99) {
                     new_problem();
                 } else {
                     draw_stack(idx++);
-                    os_SetListDim(ti_L1, idx);
-                    os_SetRealListElement(ti_L1, idx, &stack[idx - 1]);
+                    L1[idx];
+                    L1[idx] = stack[idx - 1];
                     new_entry();
                 }
             } else if (key == sk_Mode) {
@@ -356,38 +379,38 @@ int main() {
             } else if (key == sk_Del) {
                 new_problem();
             } else if (key == sk_Add) {
-                BINARY_OP(os_RealAdd);
+                BINARY_OP(add);
             } else if (key == sk_Sub) {
-                BINARY_OP(os_RealSub);
+                BINARY_OP(subtract);
             } else if (key == sk_Mul) {
-                BINARY_OP(os_RealMul);
+                BINARY_OP(multiply);
             } else if (key == sk_Div) {
-                BINARY_OP(os_RealDiv);
+                BINARY_OP(divide);
             } else if (key == sk_Power) {
-                BINARY_OP(os_RealPow);
+                BINARY_OP(powf);
             } else if (key == sk_Log) {
-                UNARY_OP(realLogBase10);
+                UNARY_OP(log10f);
             } else if (key == sk_Ln) {
-                UNARY_OP(os_RealLog);
+                UNARY_OP(logf);
             } else if (key == sk_Sin) {
-                UNARY_OP(degRadSin);
+                UNARY_OP(sin);
             } else if (key == sk_Cos) {
-                UNARY_OP(degRadCos);
+                UNARY_OP(cos);
             } else if (key == sk_Tan) {
-                UNARY_OP(degRadTan);
+                UNARY_OP(tan);
             } else if (key == sk_Square) {
-                UNARY_OP(realSquare);
+                UNARY_OP(square);
             } else if (key == sk_Recip) {
-                UNARY_OP(os_RealInv);
+                UNARY_OP(recip);
             } else if (key == sk_Comma) {
-                BINARY_OP(realScientificNotation);
+                BINARY_OP(scientificNotation);
             } else if (key == sk_Store) {
-                if (os_RealCompare(&stack[idx], &r_0) != 0) {
-                    os_SetRealVar(ti_X, &stack[idx]);
+                if (stack[idx] != 0) {
+                    x = stack[idx];
                     hint(">");
                 } else {
                     if (idx >= 1) {
-                        os_SetRealVar(ti_X, &stack[idx - 1]);
+                        x = stack[idx - 1];
                         hint(">");
                     }
                 }
@@ -395,13 +418,13 @@ int main() {
                 constants_mode(true);
             } else if (key == sk_Prgm && idx >= 2) {
                 // Swap keys
-                real_t temp = stack[idx - 1];
+                float temp = stack[idx - 1];
                 stack[idx - 1] = stack[idx - 2];
                 stack[idx - 2] = temp;
                 draw_stack_clear(idx - 2, true);
                 draw_stack_clear(idx - 1, true);
-                os_SetRealListElement(ti_L1, idx - 1, &stack[idx - 1]);
-                os_SetRealListElement(ti_L1, idx - 2, &stack[idx - 2]);
+                L1[idx - 1] = stack[idx - 1];
+                L1[idx - 2] = stack[idx - 2];
             } else if (key == sk_Yequ) {
                 os_ClrHome();
                 os_SetCursorPos(0, 0);
